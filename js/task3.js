7851529cf75859549f41855e0b4bb56bb714a1c0
bradmatt <542365734@qq.com>
2018-04-15 17:38:23 +0800

    taskchanges2

--- js/task3.js
+++ js/task3.js
@@ -1,3 +1,27 @@
-function jumpback() {
+function jump() {
     window.location.href="https://bradmatt213.github.io/learngit/task2-js";
+}
+var i=0;
+var k=0;
+function fapai() {
+    kills=JSON.parse(localStorage.getItem('kills'));
+        i++;
+    if( i%2 !== 0 && k<kills.length) {
+        $("#num").attr("value",k+1);
+        $("#icon").attr("src","img/headicon.png");
+        $(".matt").append(kills[k]);
+        $("#fanpai").attr("value","隐藏身份并传递给"+(k+2)+"号");
+        k++;
+    }
+    else if ( i%2 === 0 && k<kills.length){
+        $("#num").attr("value",k+1);
+        $("#icon").attr("src","img/myking.png");
+        $("#fanpai").attr("value","查看"+(k+1)+"号玩家身份");
+        $(".matt").empty();
+    }
+    else{
+        $("#fanpai").attr("value","法官查看");
+
+    }
+
 }
