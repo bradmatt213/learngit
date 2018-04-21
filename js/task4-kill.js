f79aabcd3861a8224d3971db5dad5987c45199c6
bradmatt <542365734@qq.com>
2018-04-21 18:01:50 +0800

    taskchangesgf

--- js/task4-kill.js
+++ js/task4-kill.js
@@ -5,6 +5,9 @@ window.onload=function auto() {
             var txt1 = $("<div></div>").text(kills[i].identity).addClass("name");
             var txt2 = $("<div></div>").text(i + 1).addClass("num");
             var txt3 = $("<li></li>").addClass("card1");
+            if(kills.state === "dead"){
+                $(txt3).addClass("red");
+            }
             $(".content").append(txt3);
             $(txt3).append(txt1, txt2);
     }
