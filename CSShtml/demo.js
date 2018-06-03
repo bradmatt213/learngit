61f279cd7ba1d6ce01e1f6c720e7030fe608584d
bradmatt <542365734@qq.com>
2018-06-03 12:12:22 +0800

    new

--- CSShtml/demo.js
+++ CSShtml/demo.js
@@ -1,18 +1,26 @@
-function Counter() {
-    this.sum = 0;
-    this.count = 0;
-}
+var fsm = new StateMachine({
+    init: 'solid',
+    transitions: [
+        {name: 'melt', from: 'solid', to: 'liquid'},
+        {name: 'freeze', from: 'liquid', to: 'solid'},
+        {name: 'vaporize', from: 'solid', to: 'gas'},
+        {name: 'condense', from: 'gas', to: 'liquid'}
+    ],
+    methods: {
+        onMelt: function () {
+            console.log('I melted')
+        },
+        onFreeze: function () {
+            console.log('I froze')
+        },
+        onVaporize: function () {
+            console.log('I vaporized')
+        },
+        onCondense: function () {
+            console.log('I condensed')
+        }
+    }
+});
 
-Counter.prototype.add = function (array) {
-    array.forEach(function (entry) {
-        this.sum += entry;
-        ++this.count;
-    }, this);
-    //console.log(this);
-};
 
-var obj = new Counter();
-obj.add([1, 3, 5, 7]);
-
-console.log(obj.count);
-console.log(obj.sum);
+console.log(fsm.allStates());
